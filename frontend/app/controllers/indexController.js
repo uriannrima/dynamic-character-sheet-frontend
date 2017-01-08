1e26d043533406d5285076ff191b245623502563
Luciano Lima <luscious.ds@gmaiil.com>
2017-01-08 13:05:20 +0000

    Small service example

--- frontend/app/controllers/indexController.js
+++ frontend/app/controllers/indexController.js
@@ -1,22 +1,32 @@
 'use strict';
 
-var IndexControllerDefinition = function IndexController($scope, TestService) {
-  
+var IndexControllerDefinition = function IndexController($scope, $timeout, TestService) {
+
   // $scope variables are not acessible from "Controller as ctrl" "scope"...
   // Controller as ctrl = this... $scope.controllersName === this
   // When you use Controller as ctrl, you're creating a instance of this inside the $scope, which is visible when using ctrl.Variable
   // So, $scope !== this.
-  
+
   var vm = this;
-  
+
   vm.MyTitle = "This is a binded title.";
-  
-  TestService.get(function(data){
-    vm.MyTitle = data.Message;
-  });
-  
+
+  // Small example of server request using a service.
+  var makeRequestFunction = function() {
+    TestService.get(function(data) {
+      vm.MyTitle = "Something came from server... Wait for it.";
+
+      var receiveMessageFunction = function() {
+        vm.MyTitle = data.Message;
+      };
+
+      $timeout(receiveMessageFunction, 2000);
+    });
+  };
+
+  $timeout(makeRequestFunction, 1000);
 };
 
 angular.
 module('dcs-app.controllers').
-controller('IndexController', ["$scope", 'TestService', IndexControllerDefinition]);
+controller('IndexController', IndexControllerDefinition);
