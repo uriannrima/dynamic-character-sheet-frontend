5d78240b002fe5d1f8e22df81bcc586e9d868f40
Luciano Lima <luciano.lima@ingresso.com>
2017-11-19 14:18:30 -0200

    Deleting older server folder. Feathers wins.

--- _server/routes/spell-school.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-module.exports = function (app) {
-    var router = express.Router();
-
-    router.get('/', app.controllers.spellSchool.getAll);
-
-    app.use('/api/spellSchool', router);
-}
