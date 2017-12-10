d5e35da537761e01a2edc241feb75301fd3a0fb4
Luciano Lima <luciano.lima@ingresso.com>
2017-12-10 21:01:42 -0200

    Grapple!

--- frontend/src/sections/ncs/components/grapple.component.vue
+++ frontend/src/sections/ncs/components/grapple.component.vue
@@ -9,6 +9,12 @@
     <input type="text" class="common-input">
     <input type="text" class="common-input">
     <input type="text" class="common-input">
+    <span>&nbsp</span>
+    <span>Total</span>
+    <span>Base Attack<br>Bonus</span>
+    <span>Strength<br>Modifier</span>
+    <span>Size<br>Modifier</span>
+    <span>Misc<br>Modifier</span>
   </div>
 </template>
 
@@ -22,7 +28,15 @@ export default {
 .grapple-component {
   display: grid;
   grid-template-columns: 30% repeat(5, 1fr);
+  grid-template-rows: auto 25%;
   align-items: center;
   justify-items: center;
+}
+
+.grapple-component span {
+  font-size: 50%;
+  line-height: 8px;
+  text-transform: uppercase;
+  text-align: center;
 }
 </style>
