2406e4ba3ee6e17e252567aec51ce9c259a58d04
Luciano Lima <luciano.lima@ingresso.com>
2017-12-10 20:55:06 -0200

    Saving Throws.

--- frontend/src/sections/ncs/components/saving-throws.component.vue
+++ frontend/src/sections/ncs/components/saving-throws.component.vue
@@ -9,24 +9,23 @@
       <label>Misc<br>Modifier</label>
       <label>Temporary<br>Modifier</label>
     </div>
-    <div v-for="n in 3" :key="n" class="saving-throw-grid">
-      <div class="black-box saving-throw-box">
-        <label>Fortitude</label>
-        <label>(Constitution)</label>
-      </div>
-      <input type="number" class="common-input">
-      <input type="number" class="common-input">
-      <input type="number" class="common-input">
-      <input type="number" class="common-input">
-      <input type="number" class="common-input">
-      <input type="number" class="common-input">
-    </div>
+    <saving-throw-component v-for="(savingThrow, index) in character.savingThrows" :key="index" :savingThrow="savingThrow" :keyAbility="character.abilityScores.find(a => a.name === savingThrow.keyAbility)"/>
   </div>
 </template>
 
 <script>
-export default {
+import SavingThrowComponent from './saving-throw.component';
+import CharacterStore from 'Store/character.store';
 
+export default {
+  components: {
+    SavingThrowComponent
+  },
+  data() {
+    return {
+      character: CharacterStore.Instance.character
+    }
+  }
 }
 </script>
 
@@ -37,26 +36,8 @@ export default {
   align-items: center;
 }
 
-.saving-throw-grid {
-  display: grid;
-  grid-template-columns: 30% repeat(6, 1fr);
-  justify-items: center;
-  align-items: center;
-}
-
-.saving-throw-header label {
-  font-size: 50%;
-  line-height: 8px;
-  text-transform: uppercase;
-  text-align: center;
-}
-
 .saving-throws-label {
   font-size: 80% !important;
   font-weight: bold;
-}
-
-.saving-throw-box {
-  height: 120%;
 }
 </style>
