9891a45eb8131404c0973946724cc845bf21855f
Luciano Lima <luscious.ds@gmail.com>
2018-04-22 17:10:44 -0300

    Store connector using components. Works, but beforeRoute goes away.

---
+++ src/sections/CharacterSheet/CharacterStoreConnector.vue
@@ -0,0 +1,15 @@
+<script>
+import StoredComponent from 'shared/components/StoredComponent';
+import CharacterSheetModule from './Store';
+
+export default {
+  render(h) {
+    return h(StoredComponent, {
+      props: {
+        name: 'CharacterSheet',
+        module: CharacterSheetModule
+      }
+    }, this.$slots.default)
+  }
+}
+</script>
