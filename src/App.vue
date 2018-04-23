8229e0d7a021a63788bddf7486672998adac0c01
Luciano Lima <luscious.ds@gmail.com>
2018-04-23 20:14:31 -0300

    Create a fullscreen loading.
    Now using proper form to commit. And now we have a fullscreen loading as slack. Just to test out.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <layout></layout>
+  <div>
+    <full-screen-loading :loading="false">
+      <layout></layout>
+    </full-screen-loading>
+  </div>
 </template>
 
 <script>
 import Layout from './shared/sections/Layout';
+import FullScreenLoading from './shared/components/FullScreenLoadingComponent';
+
 export default {
-  components: { Layout }
+  components: { Layout, FullScreenLoading }
 }
 </script>
 
---
+++ src/shared/components/FullScreenLoadingComponent.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <transition name="loading-fade">
+      <div class="loading-wrapper"
+           v-show="loading">
+        <section class="loading">
+          This app is still under construction, so please avoid filling personal information
+          <span class="loading__author"> - Guy behind this app</span>
+          <span class="loading__anim"></span>
+        </section>
+      </div>
+    </transition>
+    <div v-show="!loading">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.loading-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.loading {
+  max-width: 50%;
+  line-height: 1.4;
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-align: center;
+}
+
+.loading__author {
+  font-weight: normal;
+  font-size: 0.9rem;
+  color: rgba(189, 189, 189, 1);
+  margin: 0.6rem 0 2rem 0;
+  display: block;
+}
+
+.loading__anim {
+  width: 35px;
+  height: 35px;
+  display: inline-block;
+  border: 5px solid rgba(189, 189, 189, 0.25);
+  border-left-color: rgba(3, 155, 229, 1);
+  border-top-color: rgba(3, 155, 229, 1);
+  border-radius: 50%;
+  animation: rotate 600ms infinite linear;
+}
+
+.loading-fade-enter-active,
+.loading-fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.loading-fade-enter,
+.loading-fade-leave-to {
+  opacity: 0;
+}
+
+@keyframes rotate {
+  to {
+    transform: rotate(1turn);
+  }
+}
+</style>
