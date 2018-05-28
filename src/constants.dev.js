78abb0d5dbed02896bea701cb3e619f54707ba14
Luciano Lima <luciano.lima@ingresso.com>
2018-05-28 01:31:23 -0300

    feat(typescript): Make first step. Migrate without breaking.

--- src/constants.dev.js
+++ src/constants.dev.js
@@ -1,4 +1,4 @@
 export default {
-  SOCKET_URL: 'http://192.168.0.10:5000/',
-  API_URL: 'http://192.168.0.10:5000/'
+  SOCKET_URL: 'http://localhost:5000/',
+  API_URL: 'http://localhost:5000/'
 };
