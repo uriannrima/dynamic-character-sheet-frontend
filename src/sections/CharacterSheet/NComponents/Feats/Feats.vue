10ff0e2853f8a49f3835162eb3b23fabe713b681
Luciano Lima <luciano.lima@ingresso.com>
2018-08-03 23:48:26 -0300

    Group component.

--- src/sections/CharacterSheet/NComponents/Feats/Feats.vue
+++ src/sections/CharacterSheet/NComponents/Feats/Feats.vue
@@ -5,31 +5,31 @@
       <span v-if="feats.length <= 0"
             class="list--no-items">No feats</span>
       <div v-else
-           class="feats__groups">
+           class="groups">
         <div v-for="(feats, key) in featsGroupedByType"
              :key="key"
-             class="feats__group">
-          <div class="feats__group__header"
+             class="group">
+          <div class="group__header"
                v-show="key">
             <span>{{key}}</span>
           </div>
-          <div class="feats__group__body">
-            <div class="feats__group"
+          <div class="group__body">
+            <div class="group"
                  v-for="(feats, key) in featsGroupedBy(feats, feat => feat.class)"
                  :key="key">
-              <div class="feats__group__header"
+              <div class="group__header"
                    v-show="key">
                 <span>{{key}}</span>
               </div>
-              <div class="feats__group__body">
+              <div class="group__body">
                 <div v-for="feat in feats"
                      :key="feat._id"
-                     class="feat">
-                  <div class="feat__name">
+                     class="group-item">
+                  <div class="group-item__name">
                     <span>{{feat.name}}</span>
                     <small v-show="feat.page"> ({{feat.page}}) </small>
                   </div>
-                  <div class="feat__quick-description">
+                  <div class="group-item__quick-description">
                     <span>{{feat.quickDescription}}</span>
                   </div>
                 </div>
@@ -67,41 +67,4 @@ export default {
 
 <style lang="scss">
 @import '~public/styles/components.scss';
-
-.feats__group {
-  &__header {
-    border-bottom: solid 1px $secodary-border__color;
-    padding: 5px 0 0 5px;
-
-    span {
-      font-size: 13px;
-      color: $spells__group__header--color;
-      font-weight: bold;
-      text-transform: uppercase;
-    }
-  }
-
-  &__body {
-    padding: 5px;
-  }
-}
-
-.feat {
-  margin-bottom: 10px;
-
-  &__name {
-    font-weight: bold;
-    font-size: 13px;
-
-    small {
-      white-space: nowrap;
-      font-style: italic;
-      color: $secondary-value__color;
-    }
-  }
-
-  &__quick-description {
-    font-size: 12px;
-  }
-}
 </style>
