a7d23df500cffb785e38676e2450767f1a51e9cc
Luciano Lima <luciano.lima@ingresso.com>
2018-09-18 00:20:40 -0300

    Doc.

--- src/components/pure/ThemeProvider.tsx
+++ src/components/pure/ThemeProvider.tsx
@@ -1,5 +1,11 @@
 import Vue, { PropOptions } from 'vue'
 
+/**
+ * Generate a theme provider.
+ * @param themeProps Property validation for the Theme. Must follow the "default" Vue property declaration.
+ *  For example, the default value is { type:Object, required: true }.
+ *  But an VueType validation can also be used for a better props validation.
+ */
 export const createThemeProvider = (themeProps: PropOptions = { type: Object, required: true }) => {
   return Vue.extend({
     /**
@@ -25,8 +31,14 @@ export const createThemeProvider = (themeProps: PropOptions = { type: Object, re
   });
 }
 
+/**
+ * Generate a default theme provider.
+ */
 export const ThemeProvider = createThemeProvider();
 
+/**
+ * Theme Injector Base.
+ */
 const ThemeInjectorBase = {
   /**
    * Inject the theme
@@ -38,6 +50,10 @@ const ThemeInjectorBase = {
   }
 };
 
+/**
+ * High-Order-Component that injects theme as a property of the component passed as paramenter.
+ * @param Component Component that will be wrapped.
+ */
 export const withTheme = (Component: any) => {
   return Vue.extend({
     ...ThemeInjectorBase,
@@ -58,6 +74,10 @@ export const withTheme = (Component: any) => {
   })
 };
 
+/**
+ * Slot-scope version of the theme injector.
+ * Theme will be passed as "theme" inside of the slot-scope.
+ */
 export const ThemeInjector = Vue.extend({
   ...ThemeInjectorBase,
   render() {
