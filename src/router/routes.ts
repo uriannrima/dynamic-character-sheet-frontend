183eda77aa8a374c653f9708206a96eee2031b67
Luciano Lima <luciano.lima@ingresso.com>
2018-09-29 20:12:27 -0300

    Auto-configure routes.

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,18 +1,24 @@
-// Lazy Load Components
-import { Route as LoginRoute } from '@/pages/Login';
-import { Route as HomeRoute } from '@/pages/Home';
-import { Route as CharacterSheetRoutes } from '@/pages/CharacterSheet';
-import { Route as SandboxRoute } from '@/pages/Sandbox';
-
 const routes = [
-  ...LoginRoute,
-  ...HomeRoute,
-  ...CharacterSheetRoutes,
-  ...SandboxRoute,
   {
     path: '*',
     redirect: '/login'
   }
 ]
+
+const routeRequire = (require as any).context(
+  '@/pages/',
+  true,
+  /[Rr]oute.(js|ts)$/
+);
+
+routeRequire.keys().forEach((fileName: string) => {
+  const routeConfig = routeRequire(fileName);
+  const configuredRoutes = routeConfig.default || routeConfig.routes || routeConfig;
+  if (configuredRoutes && Array.isArray(configuredRoutes)) {
+    configuredRoutes.forEach(route => {
+      routes.unshift(route);
+    })
+  }
+})
 
 export default routes
